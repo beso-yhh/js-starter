57721da5c98b3001d9f8e6c7622245ccfe226196
beso-pro <basel.hjz1999@gmail.com>
2024-01-16 15:15:39 +0400

    Products fetching

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,81 @@
-import { greetUser } from '$utils/greet';
+// import { greetUser } from '$utils/greet';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-  document.body.style.backgroundColor = 'blue';
-});
+// window.Webflow ||= [];
+// window.Webflow.push(() => {
+//   const name = 'John Doe';
+//   greetUser(name);
+//   document.body.style.backgroundColor = 'blue';
+// });
+
+import type { Product } from './cms/populate-external-data/types';
+import type { CMSList } from './types/CMSList';
+
+/**
+ * Populate CMS Data from an external API.
+ */
+window.fsAttributes = window.fsAttributes || [];
+window.fsAttributes.push([
+  'cmsload',
+  async (listInstances: CMSList[]) => {
+    document.body.style.backgroundColor = 'red';
+    // Get the list instance
+    const [listInstance] = listInstances;
+
+    // Save a copy of the template
+    const [firstItem] = listInstance.items;
+    const itemTemplateElement = firstItem.element;
+
+    // Fetch external data
+    const products = await fetchProducts();
+
+    // Remove existing items
+    listInstance.clearItems();
+
+    // Create the new items
+    const newItems = products.map((product) => createItem(product, itemTemplateElement));
+
+    // Populate the list
+    await listInstance.addItems(newItems);
+  },
+]);
+
+/**
+ * Fetches fake products from Fake Store API.
+ * @returns An array of {@link Product}.
+ */
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data: Product[] = await response.json();
+
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+
+/**
+ * Creates an item from the template element.
+ * @param product The product data to create the item from.
+ * @param templateElement The template element.
+ *
+ * @returns A new Collection Item element.
+ */
+const createItem = (product: Product, templateElement: HTMLDivElement) => {
+  // Clone the template element
+  const newItem = templateElement.cloneNode(true) as HTMLDivElement;
+
+  // Query inner elements
+  // const image = newItem.querySelector<HTMLImageElement>('[data-element="image"]');
+  const title = newItem.querySelector<HTMLHeadingElement>('[data-element="title"]');
+  const category = newItem.querySelector<HTMLDivElement>('[data-element="category"]');
+  const description = newItem.querySelector<HTMLParagraphElement>('[data-element="description"]');
+
+  // Populate inner elements
+  // if (image) image.src = product.image;
+  if (title) title.textContent = product.title;
+  if (category) category.textContent = product.category;
+  if (description) description.textContent = product.description;
+
+  return newItem;
+};
