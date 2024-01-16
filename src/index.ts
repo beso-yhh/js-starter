b5ea4a5a38ed93001f46225ffd98e52cff3f937d
beso-pro <basel.hjz1999@gmail.com>
2024-01-16 15:05:12 +0400

    Some fixes

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ window.Webflow ||= [];
 window.Webflow.push(() => {
   const name = 'John Doe';
   greetUser(name);
-  document.body.style.backgroundColor = 'red';
+  document.body.style.backgroundColor = 'blue';
 });
